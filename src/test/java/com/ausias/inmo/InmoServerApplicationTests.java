3b6ab0b6899e4a2e04e53bf480613793febda714
perevill <pevipey@gmail.com>
2022-01-10 09:26:16 +0100

    initial commit

---
+++ src/test/java/com/ausias/inmo/InmoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ausias.inmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
