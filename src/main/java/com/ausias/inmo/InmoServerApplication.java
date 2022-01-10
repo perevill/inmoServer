8e900494980166f0da73c4bba621e28b60ae73ab
perevill <pevipey@gmail.com>
2022-01-10 09:46:44 +0100

    inicial

---
+++ src/main/java/com/ausias/inmo/InmoServerApplication.java
@@ -0,0 +1,13 @@
+package com.ausias.inmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmoServerApplication.class, args);
+	}
+
+}
