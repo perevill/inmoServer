d52294b0c8930917849e41864087214ba7d95a99
perevill <pevipey@gmail.com>
2022-01-20 11:35:16 +0100

    comarca

---
+++ src/main/java/com/ausias/inmo/entity/ComarcaEntity.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ausias.inmo.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author a023849364c
+ */
+@Entity
+@Table(name = "ciudad")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class CiudadEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+
+    @OneToMany(mappedBy = "ciudad")
+    private List<ZonaEntity> zonas = new ArrayList<>();
+
+    public CiudadEntity() {
+    }   
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getZonas() {
+        return zonas.size();
+    }
+}
---
+++ src/main/java/com/ausias/inmo/repository/ComarcaRepository.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ausias.inmo.repository;
+
+import com.ausias.inmo.entity.CiudadEntity;
+import com.ausias.inmo.entity.TipoViviendaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author a023849364c
+ */
+public interface CiudadRepository extends JpaRepository<CiudadEntity, Long>{
+        public Page<CiudadEntity> findByNombreIgnoreCaseContaining(String strFilter, Pageable oPageable);
+
+    public Page<CiudadEntity> findById(Long longCiudad, Pageable oPageable);
+
+    public Page<CiudadEntity> findByIdAndNombreIgnoreCaseContaining(Long longCiudad, String strFilter, Pageable oPageable);
+    
+}
